c84b9be580659ae508a65390513cacf1426eb588
mittens417 <cadelazaro97@gmail.com>
2025-07-01 15:04:39 +0800

    Final Submission, Celsius to Fahrenheit Converter

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,16 @@
 package org.example;
-
+import java.util.Scanner;
 import java.util.*;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
+        System.out.print("Enter temperature in Celsius: ");
+
+        Scanner scan = new Scanner(System.in);
+        float C = scan.nextFloat();
+        float F = (C * (9.0f/5.0f)) + 32;
+
+        System.out.printf("Temperature in Fahrenheit: %.2f", F);
     }
 }
